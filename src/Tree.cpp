59476406dfb85e2faa61a397681c5e0b21f036da
Pierre Barbera <pierre.pbdas@gmail.com>
2015-11-17 14:38:00 +0000

    misc refactoring, added more nonfunctional code to placement

--- src/Tree.cpp
+++ src/Tree.cpp
@@ -9,53 +9,57 @@
 
 using namespace std;
 
-Tree::Tree(const string& tree_file, const string& msa_file, Model& model) : model(model)
+Tree::Tree(const string& tree_file, const string& msa_file, Model& model) : model_(model)
 {
 
   // TODO handle filetypes other than newick/fasta
 
 
   //parse, build tree
-  msa = build_MSA_from_file(msa_file);
+  ref_msa_ = build_MSA_from_file(msa_file);
 
   build_partition_from_file(tree_file);
+
+  link_tree_msa();
+
+  precompute_clvs();
 }
 
 Tree::~Tree()
 {
   // free data segment of tree nodes
-  free_node_data(tree);
+  free_node_data(tree_);
 
-	pll_partition_destroy(partition);
+	pll_partition_destroy(partition_);
 
 }
 
 void Tree::build_partition_from_file(const string& tree_file)
 {
-  int num_tip_nodes, num_nodes, num_inner_nodes;
+  int  num_nodes, num_inner_nodes;
 
   /* first we call the appropriate pll parsing function to obtain a pll_utree structure,
     on which our partition object will be based */
-  tree = pll_utree_parse_newick(tree_file.c_str(), &num_tip_nodes);
+  tree_ = pll_utree_parse_newick(tree_file.c_str(), &num_tip_nodes_);
 
-  if (num_tip_nodes < 3)
+  if (num_tip_nodes_ < 3)
     throw runtime_error{"Number of tip nodes too small"};
 
-  set_missing_branch_length(tree, DEFAULT_BRANCH_LENGTH);
+  set_missing_branch_length(tree_, DEFAULT_BRANCH_LENGTH);
 
   // we then derive some numbers about the graph
-  num_inner_nodes = num_tip_nodes - 2;
-  num_nodes = num_inner_nodes + num_tip_nodes;
-  num_branches = num_nodes - 1;
+  num_inner_nodes = num_tip_nodes_ - 2;
+  num_nodes = num_inner_nodes + num_tip_nodes_;
+  num_branches_ = num_nodes - 1;
 
-  partition = pll_partition_create(num_tip_nodes,
+  partition_ = pll_partition_create(num_tip_nodes_,
                                    num_inner_nodes * 3, //number of extra clv buffers: 3 for every direction on the node
                                    STATES,
-                                   msa.num_sites(),
+                                   ref_msa_.num_sites(),
                                    1,
-                                   num_branches,
+                                   num_branches_,
                                    RATE_CATS,
-                                   num_inner_nodes * 3,
+                                   num_inner_nodes * 3, /* number of extra scaler buffers */
                                    PLL_ATTRIB_ARCH_SSE);
 
   double rate_cats[RATE_CATS] = {0};
@@ -66,37 +70,33 @@ void Tree::build_partition_from_file(const string& tree_file)
   pll_compute_gamma_cats(1, 4, rate_cats);
 
   /* set frequencies at model with index 0 */
-  pll_set_frequencies(partition, 0, &(model.base_frequencies()[0]));
+  pll_set_frequencies(partition_, 0, &(model_.base_frequencies()[0]));
 
   /* set substitution parameters at model with index 0 */
-  pll_set_subst_params(partition, 0, &(model.substitution_rates()[0]));
+  pll_set_subst_params(partition_, 0, &(model_.substitution_rates()[0]));
 
   /* set rate categories */
-  pll_set_category_rates(partition, rate_cats);
-
-  link_tree_msa(num_tip_nodes, tree);
+  pll_set_category_rates(partition_, rate_cats);
 
   // TODO reference part of the MSA can now be freed
-
-  precompute_clvs(num_tip_nodes, tree);
 }
 
-void Tree::link_tree_msa(const int num_tip_nodes, pll_utree_t* tree)
+void Tree::link_tree_msa()
 {
   // next, we obtain pointers to all tip nodes
-  auto tip_nodes = new pll_utree_t*[num_tip_nodes];
-  pll_utree_query_tipnodes(tree, tip_nodes);
+  auto tip_nodes = new pll_utree_t*[num_tip_nodes_];
+  pll_utree_query_tipnodes(tree_, tip_nodes);
 
   /*
   // and associate the sequences from the MSA file with the correct tips
   // TODO this is n^2. potential performance hog
   bool found = false;
-  for (int i = 0; i < num_tip_nodes; ++i)
+  for (int i = 0; i < num_tip_nodes_; ++i)
   {
     string header, sequence;
     tie(header, sequence) = msa->get(i);
 
-    for (int j = 0; j < num_tip_nodes && !found; ++j)
+    for (int j = 0; j < num_tip_nodes_ && !found; ++j)
     {
       if(header.compare(tip_nodes[j]->label) == 0)
       {
@@ -109,12 +109,12 @@ void Tree::link_tree_msa(const int num_tip_nodes, pll_utree_t* tree)
     found = false;
   }*/
 
-  /* create a libc hash table of size num_tip_nodes */
-  hcreate(num_tip_nodes);
+  /* create a libc hash table of size num_tip_nodes_ */
+  hcreate(num_tip_nodes_);
 
   /* populate a libc hash table with tree tip labels */
-  auto data = new int[num_tip_nodes];
-  for (int i = 0; i < num_tip_nodes; ++i)
+  auto data = new int[num_tip_nodes_];
+  for (int i = 0; i < num_tip_nodes_; ++i)
   {
     data[i] = i;
     ENTRY entry;
@@ -124,7 +124,7 @@ void Tree::link_tree_msa(const int num_tip_nodes, pll_utree_t* tree)
   }
 
   /* find sequences in hash table and link them with the corresponding taxa */
-  for (auto s : msa)
+  for (auto s : ref_msa_)
   {
     ENTRY query;
     query.key = &(s.header()[0]); //to get char* from std::string
@@ -137,7 +137,7 @@ void Tree::link_tree_msa(const int num_tip_nodes, pll_utree_t* tree)
 
     int tip_clv_index = *((int *)(found->data));
 
-    pll_set_tip_states(partition, tip_clv_index, pll_map_nt, s.sequence().c_str());
+    pll_set_tip_states(partition_, tip_clv_index, pll_map_nt, s.sequence().c_str());
   }
 
   /* destroy hash table and free related data */
@@ -146,31 +146,31 @@ void Tree::link_tree_msa(const int num_tip_nodes, pll_utree_t* tree)
   delete [] data;
 }
 
-void Tree::precompute_clvs(int num_tip_nodes, pll_utree_t* tree)
+void Tree::precompute_clvs()
 {
   //rederive the numbers
-  auto num_inner_nodes = num_tip_nodes - 2;
-  auto num_nodes = num_inner_nodes + num_tip_nodes;
-  num_branches = num_nodes - 1;
+  auto num_inner_nodes = num_tip_nodes_ - 2;
+  auto num_nodes = num_inner_nodes + num_tip_nodes_;
+  //num_branches_ = num_nodes - 1;
 
   int num_matrices, num_ops;
 
   /* buffer for creating a postorder traversal structure */
   auto travbuffer = new pll_utree_t*[num_nodes];
-  auto branch_lengths = new double[num_branches];
-  auto matrix_indices = new int[num_branches];
+  auto branch_lengths = new double[num_branches_];
+  auto matrix_indices = new int[num_branches_];
   auto operations = new pll_operation_t[num_nodes];
   // TODO num_nodes too many? whats the upper bound considering tip traversal?
 
   // get a list of all tip nodes
   // TODO refactor candidate
-  auto tip_nodes = new pll_utree_t*[num_tip_nodes];
-  pll_utree_query_tipnodes(tree, tip_nodes);
+  auto tip_nodes = new pll_utree_t*[num_tip_nodes_];
+  pll_utree_query_tipnodes(tree_, tip_nodes);
 
   /* adjust clv indices such that every direction has its own */
-  set_unique_clv_indices(tree, num_tip_nodes);
+  set_unique_clv_indices(tree_, num_tip_nodes_);
 
-  for (int i = 0; i < num_tip_nodes; ++i)
+  for (int i = 0; i < num_tip_nodes_; ++i)
   {
     /* perform a partial postorder traversal of the unrooted tree */
 
@@ -191,7 +191,7 @@ void Tree::precompute_clvs(int num_tip_nodes, pll_utree_t* tree)
                                 &num_matrices,
                                 &num_ops);
 
-    pll_update_prob_matrices(partition,
+    pll_update_prob_matrices(partition_,
                              0,             // use model 0
                              matrix_indices,// matrices to update
                              branch_lengths,
@@ -199,7 +199,7 @@ void Tree::precompute_clvs(int num_tip_nodes, pll_utree_t* tree)
 
     /* use the operations array to compute all num_ops inner CLVs. Operations
        will be carried out sequentially starting from operation 0 towrds num_ops-1 */
-    pll_update_partials(partition, operations, num_ops);
+    pll_update_partials(partition_, operations, num_ops);
   }
 
   delete [] travbuffer;
@@ -209,25 +209,32 @@ void Tree::precompute_clvs(int num_tip_nodes, pll_utree_t* tree)
 
 }
 
-void Tree::place(const Sequence &s) const
+void Tree::place(const MSA &msa) const
 {
-  s.sequence();
-  // place s on every edge
-  auto node_list = new pll_utree_t*[num_branches];
-  utree_query_branches(tree, node_list);
+  // get all edges
+  auto node_list = new pll_utree_t*[num_branches_];
+  auto num_traversed = utree_query_branches(tree_, node_list);
+
+  // TODO create output class
+
+  // place all s on every edge
+  for (auto s : msa)
+    for (int i = 0; i < num_traversed; ++i) {
+      place_on_edge(s, node_list[i]);
+    }
 }
 
 /* Compute loglikelihood of a Sequence, when placed on the edge defined by node */
-double Tree::place_on_edge(Sequence& s, pll_utree_t * node)
+double Tree::place_on_edge(Sequence& s, pll_utree_t * node) const
 {
   s.sequence();
   // create new tiny tree including the given nodes and a new node for the sequence s
 
   // compute the loglikelihood
-  return pll_compute_root_loglikelihood(partition, node->clv_index, node->scaler_index, 0);
+  return pll_compute_root_loglikelihood(partition_, node->clv_index, node->scaler_index, 0);
 }
 
 void Tree::visit(std::function<void(pll_partition_t *, pll_utree_t *)> f)
 {
-  f(partition, tree);
+  f(partition_, tree_);
 }
--- src/Tree.hpp
+++ src/Tree.hpp
@@ -16,21 +16,27 @@ public:
   ~Tree();
   // TODO should return placement object
   // TODO doesnt follow parallelization scheme: overload?
-  void place(const Sequence& s) const;
+  void place(const MSA& msa) const;
   // TODO bad: breaks encapsulation
   void visit(std::function<void(pll_partition_t *, pll_utree_t *)> f);
 
 private:
-  pll_partition_t * partition;
-  pll_utree_t * tree;
-  int num_branches;
-  MSA msa;
-  Model model;
+  // pll structures
+  pll_partition_t * partition_;
+  pll_utree_t * tree_; // must be top level node as parsed in newick! (for jplace)
 
-  double place_on_edge(Sequence& s, pll_utree_t * node);
+  // tree related numbers
+  int num_branches_;
+  int num_tip_nodes_;
+
+  // epa related classes
+  MSA ref_msa_;
+  Model model_;
+
+  double place_on_edge(Sequence& s, pll_utree_t * node) const;
   void build_partition_from_file(const std::string& tree_file);
-  void link_tree_msa(const int num_tip_nodes, pll_utree_t* tree);
-  void precompute_clvs(int num_tip_nodes, pll_utree_t* tree);
+  void link_tree_msa();
+  void precompute_clvs();
 
 };
 
