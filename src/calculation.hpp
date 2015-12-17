82e8d4ee621ae46d51785e8ee2e872398e3fefb2
Pierre Barbera <pierre.pbdas@gmail.com>
2015-12-17 10:43:47 +0100

    fixed typo

--- src/calculation.hpp
+++ src/calculation.hpp
@@ -1,7 +1,7 @@
 #ifndef EPA_CALCULATION_H_
 #define EPA_CALCULATION_H_
 
-#include "PQuery_set.hpp"
+#include "PQuery_Set.hpp"
 
 void compute_and_set_lwr(PQuery_Set& pqs);
 void discard_by_support_threshold(PQuery_Set& pqs, const double thresh);
