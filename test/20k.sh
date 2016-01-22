24dae64a50f1d2fefc5fb79da6e1a53b8d896e0f
Pierre Barbera <pierre.pbdas@gmail.com>
2016-01-22 21:57:14 +0100

    changed 20k.sh to actually work

--- test/20k.sh
+++ test/20k.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-OUT="lou"
+OUT="20k"
+TREE="data/lucas/20k.newick"
+MSA="data/lucas/20k.fasta"
 mkdir -p $OUT
 rm -rf $OUT/*
-./leave_one_out.py ../../standard-RAxML/raxmlHPC ../bin/epa data/lucas/tree.newick data/lucas/msa.fasta $OUT $1
+
+echo "RUNNING RAXML"
+../../standard-RAxML/raxmlHPC -f v -s $MSA -t $TREE -n 20k -m GTRGAMMA -w $OUT
+
+echo "RUNNING EPA"
+../bin/epa $TREE $MSA -O -w $OUT
+
+echo "RUNNING JPLACE_COMPARE"
+./jplace_compare.py -v $OUT/RAxML_portableTree.20k.jplace $OUT/epa_result.jplace
