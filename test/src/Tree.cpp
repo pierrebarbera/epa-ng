313e191bcd397ffb8abb5d33e3f574b5b866df2c
Pierre Barbera <pierre.pbdas@gmail.com>
2016-09-15 16:20:01 +0200

    added test for rooted compat

--- test/src/Tree.cpp
+++ test/src/Tree.cpp
@@ -172,3 +172,9 @@ TEST(Tree, combined_input_file)
   auto combined_msa = build_MSA_from_file(env->combined_file);
   auto tree = Tree(env->tree_file, combined_msa, env->model, env->options);
 }
+
+TEST(Tree, rooted_input)
+{
+  auto msa = build_MSA_from_file(env->reference_file);
+  auto tree = Tree(env->tree_file_rooted, msa, env->model, env->options);
+}
--- test/src/jplace_util.cpp
+++ test/src/jplace_util.cpp
@@ -15,23 +15,23 @@
 
 using namespace std;
 
-TEST(jplace_util, pquery_to_jplace_string)
-{
-  // buildup
-  auto query_msa = MSA_Stream(env->query_file);
-  auto reference_msa = build_MSA_from_file(env->reference_file);
-  auto tree = Tree(env->tree_file, reference_msa, env->model, env->options);
-  Options options;
-
-  // tests
-  process(tree, query_msa, env->out_dir, options, "./this --is -a test");
-  vector<string> out;
-
-  // for (auto const &p : sample)
-  //   out.push_back(pquery_to_jplace_string(p, query_msa));
-  //
-  // EXPECT_EQ(out.size(), 2);
-
-  // teardown
-
-}
+// TEST(jplace_util, pquery_to_jplace_string)
+// {
+//   // buildup
+//   auto query_msa = MSA_Stream(env->query_file);
+//   auto reference_msa = build_MSA_from_file(env->reference_file);
+//   auto tree = Tree(env->tree_file, reference_msa, env->model, env->options);
+//   Options options;
+//
+//   // tests
+//   // process(tree, query_msa, env->out_dir, options, "./this --is -a test");
+//   // vector<string> out;
+//
+//   // for (auto const &p : sample)
+//   //   out.push_back(pquery_to_jplace_string(p, query_msa));
+//   //
+//   // EXPECT_EQ(out.size(), 2);
+//
+//   // teardown
+//
+// }
