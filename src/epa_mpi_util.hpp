77d6c108cbdd8686f1a8d77074078db951f074cc
Pierre Barbera <pierre.pbdas@gmail.com>
2016-09-20 16:24:44 +0200

    templatized mpi send/recv funcs

--- src/epa_mpi_util.hpp
+++ src/epa_mpi_util.hpp
@@ -1,9 +1,86 @@
 #pragma once
 
+#ifdef __MPI
+
 #include "mpihead.hpp"
 #include "Sample.hpp"
 
-#ifdef __MPI
-void epa_mpi_send(Sample& sample, int dest_rank, MPI_Comm comm);
-void epa_mpi_recieve(Sample& sample, int source_rank, MPI_Comm comm);
+#include <sstream>
+#include <memory>
+#include <cereal/archives/binary.hpp>
+#include <cereal/types/vector.hpp>
+#include <cereal/types/string.hpp>
+
+static void err_check(int errval)
+{
+  if (errval != MPI_SUCCESS)
+  {
+    std::stringstream msg;
+    msg << "Failed MPI call: ";
+    switch (errval) {
+      case MPI_ERR_COMM:
+        msg << "Invalid communicator.";
+        break;
+      case MPI_ERR_TYPE:
+        msg << "Invalid datatype argument.";
+        break;
+      case MPI_ERR_COUNT:
+        msg << "Invalid count argument.";
+        break;
+      case MPI_ERR_TAG:
+        msg << "Invalid tag argument.";
+        break;
+      case MPI_ERR_RANK:
+        msg << "Invalid source or destination rank.";
+        break;
+      default:
+        msg << "unknown";
+    }
+    msg << std::endl;
+    throw std::runtime_error{msg.str()};
+  }
+}
+
+template <typename T>
+void epa_mpi_send(T& obj, int dest_rank, MPI_Comm comm)
+{
+  // serialize the obj
+  std::stringstream ss;
+  cereal::BinaryOutputArchive out_archive(ss);
+  out_archive(obj);
+
+  // send obj to specified node
+  std::string data = ss.str();
+  auto buffer = new char[data.size()];
+  memcpy(buffer, data.c_str(), data.size() * sizeof(char));
+  err_check(MPI_Send(buffer, data.size(), MPI_CHAR, dest_rank, 0, comm));
+  delete[] buffer;
+}
+
+template <typename T>
+void epa_mpi_recieve(T& obj, int source_rank, MPI_Comm comm)
+{
+  // probe to find out the message size
+  MPI_Status status;
+  int size;
+  err_check(MPI_Probe(source_rank, 0, comm, &status));
+  MPI_Get_count(&status, MPI_CHAR, &size);
+
+  // prepare buffer
+  auto buffer = new char[size];
+
+  //  get the actual payload
+  err_check(MPI_Recv(buffer, size, MPI_CHAR, source_rank, 0, comm, &status));
+
+  // deserialization
+  std::stringstream ss;
+  ss.write(buffer, size);
+  cereal::BinaryInputArchive in_archive(ss);
+
+  // build the object
+  in_archive(obj);
+
+  delete[] buffer;
+}
 #endif
+
