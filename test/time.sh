03e72f32f72bde648397790125796f5ee0b70e32
Pierre Barbera <barberpe@rh03664.h-its.org>
2016-10-19 16:06:59 +0200

    better time scripts (from skylake machine)

--- test/time.sh
+++ test/time.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+time=time
 ABSPATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 OUT=$ABSPATH/runtime_thorough
 LOG=$OUT/log
@@ -12,23 +13,22 @@ rm -rf $OUT/*
 touch $LOG
 
 echo "EPA" >> $LOG
-for i in {1..5}
+for i in 1
 do
-	{ time ../bin/epamk -t $TREE -s $REF_MSA -q $QRY_MSA  -w $OUT > /dev/null 2>&1 ; } 2>> $LOG
+	{ $time ../bin/epamk -t $TREE -s $REF_MSA -q $QRY_MSA  -w $OUT > /dev/null 2>&1 ; } 2>> $LOG
 done
 
-exit
 
 echo "RAXML" >> $LOG
-for i in {1..10}
+for i in 1
 do
 	rm -rf $OUT/RAxML*
-        { time ../../standard-RAxML/raxmlHPC-AVX -f v -H -s $MSA -t $TREE -n 1k -m GTRGAMMA -w $OUT > /dev/null 2>&1 ; } 2>> $LOG
+        { $time ../../standard-RAxML/raxmlHPC-AVX -f v -H -s $MSA -t $TREE -n 1k -m GTRGAMMA -w $OUT > /dev/null 2>&1 ; } 2>> $LOG
 done
 
 echo "PPLACER" >> $LOG
-for i in {1..10}
+for i in 1
 do
-        { time ./pplacer -t $TREE -s 1k/RAxML_info.1k --no-pre-mask --max-strikes 0 -j 0 --out-dir $OUT $MSA > /dev/null 2>&1 ; } 2>> $LOG
+        { $time ./pplacer -t $TREE -s 1k/RAxML_info.1k --no-pre-mask --max-strikes 0 -j 0 --out-dir $OUT $MSA > /dev/null 2>&1 ; } 2>> $LOG
 done
 
--- test/time_heur.sh
+++ test/time_heur.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+time=time
 ABSPATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 OUT=$ABSPATH/runtime_heur
 LOG=$OUT/log
@@ -11,28 +12,23 @@ mkdir -p $OUT
 rm -rf $OUT/*
 touch $LOG
 
-: <<'END'
-
 echo "EPA" >> $LOG
-for i in {1..5}
+for i in 1
 do
-	{ time ../bin/epamk -t $TREE -s $REF_MSA -q $QRY_MSA -G 0.02 -w $OUT > /dev/null 2>&1 ; } 2>> $LOG
+	{ $time ../bin/epamk -t $TREE -s $REF_MSA -q $QRY_MSA -G 0.02 -w $OUT > /dev/null 2>&1 ; } 2>> $LOG
 done
 
-exit
 
 echo "RAXML" >> $LOG
 for i in 1
 do
-	rm -rf $OUT/RAxML*
-        { time ../../standard-RAxML/raxmlHPC-AVX -f v -H -s $MSA -G 0.02 -t $TREE -n 1k -m GTRGAMMA -w $OUT > /dev/null 2>&1 ; } 2>> $LOG
+	{ $time ../../standard-RAxML/raxmlHPC-AVX -f v -H -T 1 -s $MSA -G 0.02 -t $TREE -n 1k -m GTRGAMMA -w $OUT > /dev/null 2>&1 ; } 2>> $LOG
 done
 
-END
 
 echo "PPLACER" >> $LOG
-for i in {1..5}
+for i in 1
 do
-        { time ./pplacer -t $TREE -s 1k/RAxML_info.1k --strike-box 0 --max-strikes 20 --no-pre-mask -j 0 --out-dir $OUT $MSA > /dev/null 2>&1 ; } 2>> $LOG
+        { $time ./pplacer -t $TREE -s 1k/RAxML_info.1k --strike-box 0 --max-strikes 20 --no-pre-mask -j 0 --out-dir $OUT $MSA > /dev/null 2>&1 ; } 2>> $LOG
 done
 
