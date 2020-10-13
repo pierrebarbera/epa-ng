63f4992ff18beb6fc6428435634849cc859c175b
Pierre Barbera <pierre.pbdas@gmail.com>
2020-10-13 18:55:01 +0200

    changed memsaver/branchbuffer traversal to be more sensible

--- src/tree/Tree.hpp
+++ src/tree/Tree.hpp
@@ -30,13 +30,13 @@ class Memsaver {
     // temporarily set the virtual root to one extreme of the tree (any leaf)
     // however because of the way the traverse function works, it has to be the
     // corresponding inner node
-    pll_unode_t* old_root = tree->vroot;
-    tree->vroot           = tree->nodes[ 0 ]->back;
+    // pll_unode_t* old_root = tree->vroot;
+    // tree->vroot           = tree->nodes[ 0 ]->back;
 
     // get the traversal, hopefully one that minimizes overall recomputations
     utree_query_branches( tree, &traversal_[ 0 ] );
 
-    tree->vroot = old_root;
+    // tree->vroot = old_root;
   }
 
   ~Memsaver() = default;
